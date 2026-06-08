716676d48cf878f9a3e46bc24773d68d975371fe
alisattorov06 <uzlegand.lord@gmail.com>
2026-06-08 19:21:41 +0500

    fix: Kotlin compilerOptions sintaksisi 1.9.22 ga mos

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -33,9 +33,9 @@ android {
     }
 }
 
-kotlin {
-    compilerOptions {
-        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+    kotlinOptions {
+        jvmTarget = "17"
     }
 }
 
